d77761de79a479b934ad537c5e11a0050a5f8a50
itsbot <patrickiannini@knights.ucf.edu>
2021-09-09 15:26:22 -0400

    Initial commit

---
+++ src/main/java/Exercise02/Exercise02.java
@@ -0,0 +1,13 @@
+/*
+ *  UCF COP3330 Fall 2021 Exercise 2 Solution
+ *  Copyright 2021 Patrick Iannini
+ */
+package Exercise02;
+import java.util.Scanner;
+
+
+public class Exercise02 {
+    public static void main(String[] args) {
+
+    }
+}
